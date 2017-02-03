16efeb42a83acd1d4f4cc3a7947a9ce4805a3474
Britton Kowalk <bkowalk@192-168-0-40.d08cda9561244303b4c0f9bb4bf0a955.plex.direct>
2017-02-03 09:49:17 -0700

    adding cron

--- cron.sh
+++ cron.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/node /Users/bkowalk/Library/Hopper/flight-deals/hopper.js
+/usr/local/bin/node /Users/bkowalk/Library/Hopper/flight-deals/hopper.js >> /Users/bkowalk/Library/Hopper/hopper.log 2>&1
