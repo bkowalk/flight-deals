1856144561adb3f4a70f260a11f86b0aa689f622
Britton Kowalk <brittonkowalk@Brittons-MacBook-Pro.local>
2017-02-17 07:47:10 -0700

    search string changed, pulled into var.  Writefile now adds txt for GUI visibility.  Added links for Google Maps and Hopper in emails.  Notifications now only fired for 15% price drops.

--- hopper.js
+++ hopper.js
@@ -10,7 +10,7 @@ console.log(new Date().toISOString());
 var transporter = nodemailer.createTransport("SMTP",{
     service: 'gmail',
     auth: {
-        user: 'me@gmail.com',
+        user: 'my@email.com',
         pass:'########'
     }
 });
@@ -18,9 +18,10 @@ var transporter = nodemailer.createTransport("SMTP",{
 //other config
 var toString = "my@email.com";
 var fromString = "my@email.com";
+var searchUrl = "http://www.hopper.com/flights/feed?departure_day=&departure_flex=3%3A3&destination=region%2F26m5%2Cregion%2F25Cs%2Cregion%2F20oH%2Cregion%2F2CIv%2Cregion%2F1uw0%2Cregion%2F22wS%2Cregion%2F2FyS%2Cstate%2F1y08&origin=airport%2FSLC&return_day=&return_flex=3%3A3&deal_level=10&sort_by=recent&max_price=1200&stay=5-20&departure_month=";
 var dataStoreDir = "/Users/bkowalk/Library/Hopper/store/";
 var options = {
-    url:'http://www.hopper.com/flights/feed?departure_day=&departure_flex=3%3A3&destination=region%2F26m5%2Cregion%2F25Cs%2Cregion%2F20oH%2Cregion%2F2CIv%2Cregion%2F1uw0&origin=airport%2FSLC&return_day=&return_flex=3%3A3&deal_level=10&sort_by=recent&max_price=1250&stay=8-20&departure_month=',
+    url: searchUrl,
     headers:{
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36'
     }
@@ -29,23 +30,26 @@ var options = {
 //in case we ever want to save timing too:  + "\n" + Math.floor(Date.now()/1000/86400),
 
 function storeNewBestFlight(airportCode,destinationName,price){
+	var writeString = price + "\n" + destinationName;
     fs.writeFile(
-        dataStoreDir + airportCode, 
-        price,
+        dataStoreDir + airportCode + ".txt", 
+        writeString,
         function(err){
             if (err){return console.log(err);}
             console.log("File saved for " + airportCode + " (" + destinationName + ")");
         });
 }
 
-function fireNotification(airportCode,destinationName,price,isNew){
-    var subjectString = "Alert: $" + price + " to " + destinationName;
+function fireNotification(airportCode,destinationName,price,oldPrice,isNew){
+    var subjectString = "Alert: $" + price + " to " + destinationName + " (" + airportCode + ")";
     if (isNew){ subjectString = "New City: $" + price + " to " + destinationName; }
     transporter.sendMail({
        from: fromString,
-       to: toString,
+       bcc: toString,
        subject: subjectString,
-       text: "http://www.hopper.com/flights/feed?departure_day=&departure_flex=3%3A3&destination=region%2F26m5%2Cregion%2F25Cs%2Cregion%2F20oH%2Cregion%2F2CIv%2Cregion%2F1uw0&origin=airport%2FSLC&return_day=&return_flex=3%3A3&deal_level=10&sort_by=recent&max_price=1250&stay=8-20&departure_month="
+       html: "<p>Previous best: $" + oldPrice + "</p>"
+            + "<p><a href='https://www.google.com/maps/search/" + destinationName.replace(/,/g,"").replace(/ /g,"+") + "'>Google Maps</a></p>"
+            + "<p><a href='" + searchUrl +"'>Hopper</a></p>"
     }, function(error, response){
         if(error){
             console.log(error);
@@ -60,21 +64,27 @@ function processFlightData(airportCode,destinationName,price){
     console.log("Saw " + airportCode + " - " + destinationName + " for " + price);
     
     //check if we've seen this before.
-    fs.exists(dataStoreDir + airportCode, function(exists){
+    fs.exists(dataStoreDir + airportCode + ".txt", function(exists){
         if (exists){
             //check price.  If this price is higher, do nothing.  If lower, store and send notification.
-            var storedPriceFile = fs.readFileSync(dataStoreDir + airportCode, 'utf8');
+            var storedPriceFile = fs.readFileSync(dataStoreDir + airportCode + ".txt", 'utf8');
             var storedPrice = storedPriceFile.split("\n")[0];
-            if (parseInt(price,10) < parseInt(storedPrice,10)){
+            if (storedPrice == ""){
+                console.log("Error - stored price for " + airportCode + " invalid");
+            } else if (parseInt(price,10) < parseInt(storedPrice,10)){
                 console.log ("New best for " + airportCode + "! Old best: " + storedPrice + ". New price: " + price);
                 storeNewBestFlight(airportCode, destinationName, price);
-                fireNotification(airportCode, destinationName, price, false);
+                if (price < storedPrice * .85){ //only fire for big deal drops (15% off of more)!
+                    fireNotification(airportCode, destinationName, price, storedPrice, false);
+                }
             }
         } else{
             //write out file.  No notifications for a city only seen once.
             console.log(airportCode + " is new.");
-            storeNewBestFlight(airportCode,destinationName,price);
-            //fireNotification(airportCode, destinationName, price, true);
+            //storeNewBestFlight(airportCode,destinationName,price);
+            /*if (price < 600){ //attempt to only show the good new ones
+                fireNotification(airportCode, destinationName, price, storedPrice, true);
+            }*/
         }
     });
 }
