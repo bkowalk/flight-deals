227e38afb5e7855900490e676333ee8d206c45a6
Britton Kowalk <bkowalk@192-168-0-40.d08cda9561244303b4c0f9bb4bf0a955.plex.direct>
2017-02-22 18:48:03 -0700

    cron updates

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /Users/bkowalk/Library/Hopper/flight-deals
-git pull
+git pull >> /Users/bkowalk/Library/Hopper/hopper.log 2>&1
 /usr/local/bin/node /Users/bkowalk/Library/Hopper/flight-deals/hopper.js >> /Users/bkowalk/Library/Hopper/hopper.log 2>&1
